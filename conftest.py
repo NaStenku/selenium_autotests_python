1e8aa024c26d6ea4fb84bfa75ba198b116677934
Anastasiia <mastenku1@gmail.com>
2022-06-13 11:45:06 +0100

    first autotest login page

---
+++ conftest.py
@@ -0,0 +1,21 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default="en",
+                     help="Choose from langs: (en/ru/es...)")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    user_language = request.config.getoption("language")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    print("\nstart browser for test.py..")
+    browser = webdriver.Chrome(options=options)
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
