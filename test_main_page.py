fac80ece9e2e85defba150e011ea242790f76608
Anastasiia <mastenku1@gmail.com>
2022-06-13 21:39:29 +0100

    test quest should see ligin link

--- test_main_page.py
+++ test_main_page.py
@@ -5,6 +5,7 @@ link = "http://selenium1py.pythonanywhere.com/"
 
 
 def test_guest_can_go_to_login_page(browser):
+
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
@@ -13,3 +14,12 @@ def test_guest_can_go_to_login_page(browser):
 def go_to_login_page(browser):
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
     login_link.click()
+
+
+def test_guest_should_see_login_link(browser):
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+
